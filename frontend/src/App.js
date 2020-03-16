91dc03ea5063ca503ba2916960a2d5593aa0641a
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-03-16 15:36:34 -0400

    initial exteral api pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import secret from './secret.js'
+let baseURL = ''
+if (process.env.NODE_ENV === 'development'){
+  baseURL = secret.apikey
+} else {
+  baseURL = 'https://movie-critique.herokuapp.com/'
 }
 
-export default App;
+
+
+
+
+ export default class App extends React.Component{
+
+   constructor(props){
+     super(props)
+     this.state = {
+       externalMovies: [],
+       storedMovies: []
+     }
+    this.getMovies = this.getMovies.bind(this)
+   }
+   componentDidMount(){
+     this.getMovies()
+   }
+   async getMovies() {
+     try {
+       let response = await fetch(`${baseURL}`)
+       let externalData = await response.json()
+       this.setState({externalMovies: externalData})
+     } catch (e) {
+       console.error(e);
+     }
+   }
+
+render(){
+  return(
+    <>
+    <h1>Movie Critique</h1>
+    <div>
+      <h2>{this.state.externalMovies.Title}</h2>
+      <img src={`${this.state.externalMovies.Poster}`} />
+    </div>
+    </>
+  )
+}
+
+
+}
