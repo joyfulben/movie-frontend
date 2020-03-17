a87cecab7d6acbed56871b8de98de7ab115c554e
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-03-17 10:52:53 -0400

    added bind for delete function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV === 'development'){
        storedMovies: []
      }
     this.getMovies = this.getMovies.bind(this)
+    this.deleteMovie = this.deleteMovie.bind(this)
    }
    componentDidMount(){
      this.getMovies()
