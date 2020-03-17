89e0b668372fb2739a409eacb53994556d301392
joyfulben <benfishernow@gmail.com>
2020-03-16 20:20:06 -0600

    Search bar renders 10 entries from external API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,69 +17,93 @@ if (process.env.NODE_ENV === 'development'){
    constructor(props){
      super(props)
      this.state = {
-       externalMovies: [],
+       externalMovies:[],
        storedMovies: []
      }
-    this.getMovies = this.getMovies.bind(this)
+    // this.getMovies = this.getMovies.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleQuery = this.handleQuery.bind(this)
    }
    componentDidMount(){
-     this.getMovies()
    }
-   async getMovies() {
+   // async getMovies() {
+   //
+   // }
+   handleChange (event) {
+     this.setState({ [event.currentTarget.id]: event.currentTarget.value})
+   }
+   async handleQuery (event) {
+     event.preventDefault()
      try {
-       let response = await fetch(`${baseURL}`)
+       let response = await fetch(`${baseURL + this.state.title}&page=1`)
        let externalData = await response.json()
        this.setState({externalMovies: externalData})
      } catch (e) {
        console.error(e);
      }
    }
-     async addMovie () {
-    try{
-      let response = await fetch(baseURL + '/movies', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: this.state.movieExternal[i].title,
-          released: this.state.movieExternal[i].released,
-          rated: this.state.movieExternal[i].rated,
-          genre: this.state.movieExternal[i].genre,
-          director: this.state.movieExternal[i].director,
-          actors: this.state.movieExternal[i].actors,
-          plot: this.state.movieExternal[i].plot,
-          poster: this.state.movieExternal[i].poster,
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      let data = await response.json()
-      const myMovies = [data, ...this.state.myMovieList]
-      this.setState({
-        myMovieList: myMovies,
-        title: '',
-        released: '',
-        rated: '',
-        genre: '',
-        director: '',
-        actors : [],
-        plot: '',
-        poster: ''
-      })
-    }catch(e){
-      console.error({'Error': e})
-    }
-  }
+  //    async addMovie () {
+  //   try{
+  //     let response = await fetch(baseURL + '/movies', {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         title: this.state.externalMovies[i].title,
+  //         released: this.state.externalMovies[i].released,
+  //         rated: this.state.externalMovies[i].rated,
+  //         genre: this.state.externalMovies[i].genre,
+  //         director: this.state.externalMovies[i].director,
+  //         actors: this.state.externalMovies[i].actors,
+  //         plot: this.state.externalMovies[i].plot,
+  //         poster: this.state.externalMovies[i].poster,
+  //       }),
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //     let data = await response.json()
+  //     const myMovies = [data, ...this.state.storedMovies]
+  //     this.setState({
+  //       storedMovies: myMovies,
+  //       title: '',
+  //       released: '',
+  //       rated: '',
+  //       genre: '',
+  //       director: '',
+  //       actors : [],
+  //       plot: '',
+  //       poster: ''
+  //     })
+  //   }catch(e){
+  //     console.error({'Error': e})
+  //   }
+  // }
 
 render(){
   return(
     <>
     <h1>Movie Critique</h1>
     <div>
-      <h2>{this.state.externalMovies.Title}</h2>
-      <img src={`${this.state.externalMovies.Poster}`} />
+      <form onSubmit={this.handleQuery}>
+        <label htmlFor="title"></label>
+        <input placeholder="Movie title: Jaws, Shrek, Batman..." type="text" id="title" name="title" onChange={this.handleChange}/>
+        <input type="submit" value="Find Movie"/>
+      </form>
+      {this.state.externalMovies.length !== 0 ?
+        <div>
+          {this.state.externalMovies.Search.map((movie, i) => {
+            return (
+              <li key={i}>
+              <h2>{movie.Title}</h2>
+              <img className="external-img" src={`${movie.Poster}`} alt="" />
+              </li>
+            )
+          })
+        }
+        </div>
+      : <div></div>
+    }
     </div>
     </>
   )
 }
-
-
+}
