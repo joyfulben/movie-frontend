105a97150b2f7b46d0898e095ae56178d9550f34
Jamal McDowell <jamal.mcdowell@gmail.com>
2020-03-17 20:48:44 -0400

    Tweaking some files

--- frontend/src/Components/ExternalMovieDisplay.js
+++ frontend/src/Components/ExternalMovieDisplay.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class SearchBar extends Component {
+export default class MovieDisplay extends Component {
     constructor(props){
         super(props)
     }
