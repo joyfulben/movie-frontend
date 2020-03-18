56a2f86dee9de3bcf7a9de94119ec4695d7adad9
Chris Bellaro <chrisbellaro@Chriss-Air.home>
2020-03-18 10:50:24 -0400

    updated schema for reviews

--- backend/models/reviews.js
+++ backend/models/reviews.js
@@ -4,7 +4,9 @@ const reviewSchema = mongoose.Schema({
     title: {type: String},
     release_date: {type: String},
     overview: {type: String},
-    poster_path: {type: String}
+    poster_path: {type: String},
+    author: {type: String},
+    review: {type: String}
 })
 
 module.exports = mongoose.model('Review', reviewSchema )
